356e29a758d09f6ab6f8f8124b35b755cb203d9a
Paul Anuschek <paul.anuschek@novatec-gmbh.de>
2023-04-12 11:44:12 +0200

    initialized spring boot project

---
+++ src/test/kotlin/de/dqualizer/dqtranslator/DqtranslatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.dqualizer.dqtranslator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DqtranslatorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
