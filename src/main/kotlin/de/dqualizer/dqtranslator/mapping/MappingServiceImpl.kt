19f92848ee376298fea6689fdf57c789f5f46243
LWagner <st148345@stud.uni-stuttgart.de>
2023-04-15 18:05:34 +0200

    Fix Resource loading

--- src/main/kotlin/de/dqualizer/dqtranslator/mapping/MappingServiceImpl.kt
+++ src/main/kotlin/de/dqualizer/dqtranslator/mapping/MappingServiceImpl.kt
@@ -5,24 +5,27 @@ import de.dqualizer.dqtranslator.ContextNotFoundException
 import dqualizer.dqlang.archive.loadtesttranslator.dqlang.domainarchitecturemapping.DomainArchitectureMapping
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.io.ResourceLoader
+import org.springframework.core.io.support.ResourcePatternUtils
 import org.springframework.stereotype.Service
-import java.io.File
-import java.nio.file.Files
 
 @Service
-class MappingServiceImpl (
-        private val objectMapper: ObjectMapper
-)  : MappingService{
+class MappingServiceImpl(
+    private val objectMapper: ObjectMapper,
+    private val resourceLoader: ResourceLoader
+) : MappingService {
     private val log = LoggerFactory.getLogger(MappingServiceImpl::class.java)
 
     @Value("\${dqualizer.mappingDirectory}")
     private lateinit var mappingDirectory: String
 
-    private val mappingPath: String
-        get() = this::class.java.classLoader.getResource("").file.substring(1) + mappingDirectory
-
     override fun getMappingByContext(context: String): DomainArchitectureMapping {
-        val json = MappingServiceImpl::class.java.getResource("/$mappingDirectory/mapping-werkstatt.json")
-        return objectMapper.readValue(json, DomainArchitectureMapping::class.java)
+        log.info("Trying to load context=$context from directory=classpath:$mappingDirectory")
+
+        return ResourcePatternUtils.getResourcePatternResolver(resourceLoader)
+            .getResources("classpath:$mappingDirectory/*.json")
+            .map { it.inputStream.bufferedReader().readText() }
+            .map { objectMapper.readValue(it, DomainArchitectureMapping::class.java) }
+            .firstOrNull { it.context == context } ?: throw ContextNotFoundException(context)
     }
 }
