e8755bd316e45fad40cf3c5ee5294035ce0b8ab9
Henning Möllers <henning.moellers@wps.de>
2024-01-02 16:08:53 +0100

    Check rqaDef for load- and resilience tests and if given, translate.

--- src/main/kotlin/de/dqualizer/dqtranslator/api/MessageController.kt
+++ src/main/kotlin/de/dqualizer/dqtranslator/api/MessageController.kt
@@ -1,41 +1,35 @@
 package de.dqualizer.dqtranslator.api
 
+
 import com.fasterxml.jackson.databind.ObjectMapper
 import de.dqualizer.dqtranslator.messaging.RQAConfigurationProducer
 import de.dqualizer.dqtranslator.translation.TranslationService
-import io.github.dqualizer.dqlang.types.adapter.constants.ResponseTime
 import io.github.dqualizer.dqlang.types.dam.Payload
 import io.github.dqualizer.dqlang.types.dam.Scenario
 import io.github.dqualizer.dqlang.types.rqa.definition.Artifact
 import io.github.dqualizer.dqlang.types.rqa.definition.RuntimeQualityAnalysis
 import io.github.dqualizer.dqlang.types.rqa.definition.RuntimeQualityAnalysisDefinition
-import io.github.dqualizer.dqlang.types.rqa.definition.enums.BaseLoad
-import io.github.dqualizer.dqlang.types.rqa.definition.enums.Environment
-import io.github.dqualizer.dqlang.types.rqa.definition.enums.LoadProfile
-import io.github.dqualizer.dqlang.types.rqa.definition.enums.ResultMetrics
-import io.github.dqualizer.dqlang.types.rqa.definition.enums.Satisfaction
+import io.github.dqualizer.dqlang.types.rqa.definition.enums.*
 import io.github.dqualizer.dqlang.types.rqa.definition.loadtest.LoadTestDefinition
 import io.github.dqualizer.dqlang.types.rqa.definition.loadtest.Parametrization
 import io.github.dqualizer.dqlang.types.rqa.definition.loadtest.ResponseMeasures
 import io.github.dqualizer.dqlang.types.rqa.definition.loadtest.stimulus.ConstantLoadStimulus
-import io.github.dqualizer.dqlang.types.rqa.definition.loadtest.stimulus.LoadStimulus
 import io.github.dqualizer.dqlang.types.rqa.definition.resiliencetest.ResilienceResponseMeasures
 import io.github.dqualizer.dqlang.types.rqa.definition.resiliencetest.ResilienceTestDefinition
 import io.github.dqualizer.dqlang.types.rqa.definition.resiliencetest.stimulus.UnavailabilityStimulus
-
-
-import org.slf4j.LoggerFactory
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RestController
-
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.PathVariable
 import io.ktor.client.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import kotlinx.coroutines.runBlocking
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+import java.io.File
+import kotlin.io.path.Path
 
 
 @RestController
@@ -61,14 +55,19 @@ class MessageController (private val translationService: TranslationService,
 
         runBlocking {
             rqaResponse = client.get("http://${dqApiHost}:${dqApiPort}/api/v1/rqa-definition/$rqaId")
-            val rqaDef = objectMapper.readValue(rqaResponse.bodyAsText(), RuntimeQualityAnalysisDefinition::class.java)
-            val loadTestConfig = translationService.translateRqaDefToLoadTestConfig(rqaDef)
-            val resilienceTestConfig = translationService.translateRqaDefToResilienceTestConfig(rqaDef)
-            log.info(loadTestConfig.loadTestArtifacts.toString())
-            log.info(resilienceTestConfig.enrichedResilienceTestDefinitions.toString())
-            rqaConfigurationProducer.produceLoadtestConfig(loadTestConfig)
-           // rqaConfigurationProducer.produce(resilienceTestConfig)
+            val rqaDefinition = objectMapper.readValue(rqaResponse.bodyAsText(), RuntimeQualityAnalysisDefinition::class.java)
 
+            if(rqaDefinition.runtimeQualityAnalysis.resilienceTests.isNotEmpty()){
+                val resilienceTestConfig = translationService.translateRqaDefToResilienceTestConfig(rqaDefinition)
+                log.info(resilienceTestConfig.enrichedResilienceTestDefinitions.toString())
+                rqaConfigurationProducer.produceResilienceTestConfig(resilienceTestConfig)
+            }
+
+            if (rqaDefinition.runtimeQualityAnalysis.loadtests.isNotEmpty()){
+                val loadTestConfig = translationService.translateRqaDefToLoadTestConfig(rqaDefinition)
+                log.info(loadTestConfig.loadTestArtifacts.toString())
+                rqaConfigurationProducer.produceLoadtestConfig(loadTestConfig)
+            }
         }
     }
 
@@ -94,8 +93,11 @@ class MessageController (private val translationService: TranslationService,
 
         val runtimeQualityAnalysis = RuntimeQualityAnalysis()
         runtimeQualityAnalysis.loadtests.add(loadTestDefinition)
-        //runtimeQualityAnalysis.resilienceTests.add(resilienceTestDefinition)
+        runtimeQualityAnalysis.resilienceTests.add(resilienceTestDefinition)
         val rqaDefinition = RuntimeQualityAnalysisDefinition("A testing rqaDef", "1", Environment.DEV, "Computer work station", "Using a password manager", runtimeQualityAnalysis)
+       // val objectMapper = ObjectMapper()
+       // objectMapper.writeValue(File("C:\\Users\\HenningMöllers\\Downloads\\rqaDef.json"), rqaDefinition)
+
 
         runBlocking {
 
