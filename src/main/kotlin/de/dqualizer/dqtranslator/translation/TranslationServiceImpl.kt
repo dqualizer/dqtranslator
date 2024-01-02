c32b5f3c73b8f97a950fe2a18b769aeae2e81c03
Henning Möllers <henning.moellers@wps.de>
2024-01-02 16:09:34 +0100

    Get DAM from DqApi instead of hardcoded DAM.

--- src/main/kotlin/de/dqualizer/dqtranslator/translation/TranslationServiceImpl.kt
+++ src/main/kotlin/de/dqualizer/dqtranslator/translation/TranslationServiceImpl.kt
@@ -26,7 +26,7 @@ class TranslationServiceImpl(
 
     override fun translateRqaDefToLoadTestConfig(rqaDefinition: RuntimeQualityAnalysisDefinition): LoadTestConfiguration {
         // Get domain architecture Mapping from Api by domain_id
-        val domainArchitectureMapping = mappingService.getMappingByIdHardcoded(rqaDefinition.domainId)
+        val domainArchitectureMapping = mappingService.getMappingById(rqaDefinition.domainId)
         val loadTestDefinition = rqaDefinition.runtimeQualityAnalysis.loadtests
 
         // Artifact will always be an Edge...
@@ -53,7 +53,7 @@ class TranslationServiceImpl(
 
     override fun translateRqaDefToResilienceTestConfig(rqaDefinition: RuntimeQualityAnalysisDefinition): ResilienceTestConfiguration {
         // Get domain architecture Mapping from Api by domain_id
-        val domainArchitectureMapping = mappingService.getMappingByIdHardcoded(rqaDefinition.domainId)
+        val domainArchitectureMapping = mappingService.getMappingById(rqaDefinition.domainId)
 
         val resilienceTestDefinitions = rqaDefinition.runtimeQualityAnalysis.resilienceTests
         val (resilienceTestDefinitionsForSystems, resilienceTestDefinitionsForActivities) = resilienceTestDefinitions.partition { it.artifact.activityId == null }
