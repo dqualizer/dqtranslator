3a43458d71d0381792e7fe32d71b827c1ea991f2
LitschiW <lion.wagner@datev.de>
2024-03-21 12:41:31 +0100

    Remove uneccessrary classes

--- src/main/kotlin/io/github/dqualizer/dqtranslator/mapping/MappingService.kt
+++
@@ -1,8 +0,0 @@
-package io.github.dqualizer.dqtranslator.mapping
-
-import io.github.dqualizer.dqlang.types.dam.DomainArchitectureMapping
-
-
-interface MappingService {
-    fun getDAMByContext(context: String): DomainArchitectureMapping
-}
