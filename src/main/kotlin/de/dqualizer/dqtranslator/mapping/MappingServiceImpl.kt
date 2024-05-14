88f9687898ee568fee26284c9ee14cc12e771a1f
Henning Möllers <LeHenningo@users.noreply.github.com>
2024-05-14 17:11:10 +0200

    Fix usage of recent refactoring.

--- src/main/kotlin/de/dqualizer/dqtranslator/mapping/MappingServiceImpl.kt
+++ src/main/kotlin/de/dqualizer/dqtranslator/mapping/MappingServiceImpl.kt
@@ -72,7 +72,7 @@ class MappingServiceImpl(
         val systemForMapping = System()
         systemForMapping.id = "MyComputer"
         systemForMapping.type = "Process"
-        systemForMapping.operationId = "KeePassXC.exe"
+        systemForMapping.processId = "KeePassXC.exe"
         systemForMapping.processPath = """C:\Program Files\KeePassXC.exe"""
         val activity = Activity()
         val endpoint = Endpoint()
