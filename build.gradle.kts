4308e406ee007225006d7742437bb44b0786adaa
Jens Plüddemann <jens.plueddemann@novatec-gmbh.de>
2024-07-03 18:19:27 +0200

    cleanup and new github action

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,103 +2,103 @@ val dqlangVersion = "3.1.14-SNAPSHOT"
 val ktorVersion = "2.3.12"
 
 plugins {
-    kotlin("jvm") version "2.0.0"
-    kotlin("plugin.spring") version "2.0.0"
-    kotlin("plugin.serialization") version "2.0.0"
+  kotlin("jvm") version "2.0.0"
+  kotlin("plugin.spring") version "2.0.0"
+  kotlin("plugin.serialization") version "2.0.0"
 
-    id("org.springframework.boot") version "3.2.5"
-    id("io.spring.dependency-management") version "1.1.5"
+  id("org.springframework.boot") version "3.2.5"
+  id("io.spring.dependency-management") version "1.1.5"
 
-    id("net.researchgate.release") version "3.0.2"
-    id("maven-publish")
-    id("idea")
-    id("eclipse")
+  id("net.researchgate.release") version "3.0.2"
+  id("maven-publish")
+  id("idea")
+  id("eclipse")
 }
 
 group = "io.github.dqualizer"
 
 release {
-    //no config needed, see https://github.com/researchgate/gradle-release for options
+  //no config needed, see https://github.com/researchgate/gradle-release for options
 }
 
 publishing {
-    repositories {
-        maven {
-            name = "gpr"
-            url = uri("https://maven.pkg.github.com/dqualizer/dqtranslator")
-            credentials(PasswordCredentials::class)
-        }
-        publications {
-            register("jar", MavenPublication::class) {
-                from(components["java"])
-                pom {
-                    url.set("https://github.com/dqualizer/dqtranslator.git")
-                }
-            }
-        }
+  repositories {
+    maven {
+      name = "gpr"
+      url = uri("https://maven.pkg.github.com/dqualizer/dqtranslator")
+      credentials(PasswordCredentials::class)
     }
+    publications {
+      register("jar", MavenPublication::class) {
+        from(components["java"])
+        pom {
+          url.set("https://github.com/dqualizer/dqtranslator.git")
+        }
+      }
+    }
+  }
 }
 
 
 configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
-    }
+  compileOnly {
+    extendsFrom(configurations.annotationProcessor.get())
+  }
 }
 
 repositories {
-    mavenCentral()
-    maven {
-        name = "gpr"
-        url = uri("https://maven.pkg.github.com/dqualizer/dqlang")
-        credentials(PasswordCredentials::class)
-    }
+  mavenCentral()
+  maven {
+    name = "gpr"
+    url = uri("https://maven.pkg.github.com/dqualizer/dqlang")
+    credentials(PasswordCredentials::class)
+  }
 }
 
 dependencies {
-    implementation(platform("org.springframework.boot:spring-boot-dependencies:3.3.1"))
+  implementation(platform("org.springframework.boot:spring-boot-dependencies:3.3.1"))
 
-    implementation("org.springframework.boot:spring-boot-starter-amqp")
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springframework.boot:spring-boot-starter-amqp")
+  implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+  implementation("org.springframework.boot:spring-boot-starter-web")
 
-    implementation("io.ktor:ktor-client-core:$ktorVersion")
-    implementation("io.ktor:ktor-client-cio:$ktorVersion")
-    implementation("io.ktor:ktor-client-logging:$ktorVersion")
+  implementation("io.ktor:ktor-client-core:$ktorVersion")
+  implementation("io.ktor:ktor-client-cio:$ktorVersion")
+  implementation("io.ktor:ktor-client-logging:$ktorVersion")
 
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
 
-    implementation("io.github.dqualizer:dqlang:$dqlangVersion")
+  implementation("io.github.dqualizer:dqlang:$dqlangVersion")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.amqp:spring-rabbit-test")
-    testImplementation("org.jeasy:easy-random-core:5.0.0")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:5.3.1")
-    //assertJ
-    testImplementation("org.assertj:assertj-core:3.25.3")
-    testImplementation("com.github.fridujo:rabbitmq-mock:1.2.0")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.springframework.amqp:spring-rabbit-test")
+  testImplementation("org.jeasy:easy-random-core:5.0.0")
+  testImplementation("org.mockito.kotlin:mockito-kotlin:5.3.1")
+  //assertJ
+  testImplementation("org.assertj:assertj-core:3.25.3")
+  testImplementation("com.github.fridujo:rabbitmq-mock:1.2.0")
 
-    //testcontainers
-    testImplementation("org.testcontainers:testcontainers")
-    testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.testcontainers:rabbitmq")
-    testImplementation("org.testcontainers:mongodb")
+  //testcontainers
+  testImplementation("org.testcontainers:testcontainers")
+  testImplementation("org.testcontainers:junit-jupiter")
+  testImplementation("org.testcontainers:rabbitmq")
+  testImplementation("org.testcontainers:mongodb")
 }
 
 kotlin {
-    compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
-    }
+  compilerOptions {
+    freeCompilerArgs.addAll("-Xjsr305=strict")
+  }
 }
 
 
 tasks.withType<Test> {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
 
 // Disable generation of "-plain" jar by the Spring Boot plugin
 tasks.getByName<Jar>("jar") {
-    enabled = false
+  enabled = false
 }
--- src/main/kotlin/io/github/dqualizer/dqtranslator/translation/translators/LoadTranslator.kt
+++ src/main/kotlin/io/github/dqualizer/dqtranslator/translation/translators/LoadTranslator.kt
@@ -15,97 +15,97 @@ import org.springframework.stereotype.Service
 
 @Service
 class LoadTranslator(
-    val damRepo: DAMMongoRepository
+  val damRepo: DAMMongoRepository
 ) : RQATranslator {
-    private val log = LoggerFactory.getLogger(javaClass)
+  private val log = LoggerFactory.getLogger(javaClass)
 
-    override fun translate(
-        rqaDefinition: RuntimeQualityAnalysisDefinition,
-        target: RQAConfiguration
-    ): RQAConfiguration {
-        target.loadConfiguration = translate(rqaDefinition)
-        return target
-    }
+  override fun translate(
+    rqaDefinition: RuntimeQualityAnalysisDefinition,
+    target: RQAConfiguration
+  ): RQAConfiguration {
+    target.loadConfiguration = translate(rqaDefinition)
+    return target
+  }
 
-    private fun translate(rqaDefinition: RuntimeQualityAnalysisDefinition): LoadTestConfiguration {
-        val dam = damRepo.findById(rqaDefinition.domainId)
-            .orElseThrow { NoSuchElementException("No DAM found with id ${rqaDefinition.domainId}") }
+  private fun translate(rqaDefinition: RuntimeQualityAnalysisDefinition): LoadTestConfiguration {
+    val dam = damRepo.findById(rqaDefinition.domainId)
+      .orElseThrow { NoSuchElementException("No DAM found with id ${rqaDefinition.domainId}") }
 
-        val loadTestDefinition = rqaDefinition.runtimeQualityAnalysis.loadTestDefinition
-        if (loadTestDefinition.isEmpty()) return LoadTestConfiguration()
+    val loadTestDefinition = rqaDefinition.runtimeQualityAnalysis.loadTestDefinition
+    if (loadTestDefinition.isEmpty()) return LoadTestConfiguration()
 
-        // Artifact will always be an Edge...
-        val (systems, activities) = loadTestDefinition.partition { it.artifact?.activityId == null }
+    // Artifact will always be an Edge...
+    val (systems, activities) = loadTestDefinition.partition { it.artifact?.activityId == null }
 
-        val loadTestConfigurations =
-            systems.map { nodeToLoadTests(it, dam) }.flatten() + activities.map { edgeToLoadTest(it, dam) }
+    val loadTestConfigurations =
+      systems.map { nodeToLoadTests(it, dam) }.flatten() + activities.map { edgeToLoadTest(it, dam) }
 
-        val loadtestConfiguration = LoadTestConfiguration(
-            rqaDefinition.version,
-            rqaDefinition.context,
-            rqaDefinition.environment.toString(),
-            dam.getEndpoint(rqaDefinition.environment.toString()),
-            loadTestConfigurations.toHashSet()
+    val loadtestConfiguration = LoadTestConfiguration(
+      rqaDefinition.version,
+      rqaDefinition.context,
+      rqaDefinition.environment.toString(),
+      dam.getEndpoint(rqaDefinition.environment.toString()),
+      loadTestConfigurations.toHashSet()
+    )
+    log.info(loadtestConfiguration.toString())
+    return loadtestConfiguration
+
+  }
+
+  fun nodeToLoadTests(
+    loadtestDefinition: LoadTestDefinition,
+    mapping: DomainArchitectureMapping
+  ): List<LoadTestArtifact> {
+    val actor = mapping.domainStory.actors.firstOrNull { it.id == loadtestDefinition.artifact?.systemId }
+    val activitiesOfSpecifiedActor = mapping.domainStory.activities.filter { it.initiators.contains(actor?.id) }
+    val loadTestArtifacts = mutableListOf<LoadTestArtifact>()
+    for (activity in activitiesOfSpecifiedActor) {
+      // Not tested
+      val endpoint = mapping.mapper.mapToArchitecturalEntity(activity) as RESTEndpoint
+      loadTestArtifacts.add(
+        LoadTestArtifact(
+          loadtestDefinition.artifact,
+          loadtestDefinition.description,
+          loadtestDefinition.stimulus,
+          loadtestDefinition.responseMeasures,
+          endpoint
         )
-        log.info(loadtestConfiguration.toString())
-        return loadtestConfiguration
-
+      )
     }
+    return loadTestArtifacts
+  }
 
-    fun nodeToLoadTests(
-        loadtestDefinition: LoadTestDefinition,
-        mapping: DomainArchitectureMapping
-    ): List<LoadTestArtifact> {
-        val actor = mapping.domainStory.actors.firstOrNull { it.id == loadtestDefinition.artifact?.systemId }
-        val activitiesOfSpecifiedActor = mapping.domainStory.activities.filter { it.initiators.contains(actor?.id) }
-        val loadTestArtifacts = mutableListOf<LoadTestArtifact>()
-        for (activity in activitiesOfSpecifiedActor) {
-            // Not tested
-            val endpoint = mapping.mapper.mapToArchitecturalEntity(activity) as RESTEndpoint
-            loadTestArtifacts.add(
-                LoadTestArtifact(
-                    loadtestDefinition.artifact,
-                    loadtestDefinition.description,
-                    loadtestDefinition.stimulus,
-                    loadtestDefinition.responseMeasures,
-                    endpoint
-                )
-            )
+  fun edgeToLoadTest(loadtestSpec: LoadTestDefinition, mapping: DomainArchitectureMapping): LoadTestArtifact {
+    return LoadTestArtifact(
+      loadtestSpec.artifact,
+      loadtestSpec.description,
+      loadtestSpec.stimulus,
+      loadtestSpec.responseMeasures,
+      loadtestSpec.getEndpoint(mapping)
+    )
+  }
+
+  private fun LoadTestDefinition.getEndpoint(mapping: DomainArchitectureMapping): RESTEndpoint {
+    val actor = mapping.domainStory.actors.first { it.id == this.artifact?.systemId }
+
+    // use it.targets instead of it.initiators
+    // e.g. Petra (initiator) calls endpoint of service (target)
+    val activityOfSpecifiedActor = mapping.domainStory.activities.first { it.targets.contains(actor.id) }
+    val entity = mapping.mapper.mapToArchitecturalEntity(activityOfSpecifiedActor)
+
+    return mapping.softwareSystem.endpoints.first { it.codeComponent == entity.id }
+  }
+
+  private fun DomainArchitectureMapping.getEndpoint(environment: String): String {
+    val services = this.softwareSystem.services
+    for (service in services) {
+      val serviceInfos = service.apiSchema!!.serverInfo
+      for (serviceInfo in serviceInfos) {
+        if (serviceInfo.environment == environment) {
+          return serviceInfo.host!!
         }
-        return loadTestArtifacts
+      }
     }
-
-    fun edgeToLoadTest(loadtestSpec: LoadTestDefinition, mapping: DomainArchitectureMapping): LoadTestArtifact {
-        return LoadTestArtifact(
-            loadtestSpec.artifact,
-            loadtestSpec.description,
-            loadtestSpec.stimulus,
-            loadtestSpec.responseMeasures,
-            loadtestSpec.getEndpoint(mapping)
-        )
-    }
-
-    private fun LoadTestDefinition.getEndpoint(mapping: DomainArchitectureMapping): RESTEndpoint {
-        val actor = mapping.domainStory.actors.first { it.id == this.artifact?.systemId }
-
-        // use it.targets instead of it.initiators
-        // e.g. Petra (initiator) calls endpoint of service (target)
-        val activityOfSpecifiedActor = mapping.domainStory.activities.first { it.targets.contains(actor.id) }
-        val entity = mapping.mapper.mapToArchitecturalEntity(activityOfSpecifiedActor)
-
-        return mapping.softwareSystem.endpoints.first { it.codeComponent == entity.id }
-    }
-
-    private fun DomainArchitectureMapping.getEndpoint(environment: String): String {
-        val services = this.softwareSystem.services
-        for (service in services) {
-            val serviceInfos = service.apiSchema!!.serverInfo
-            for (serviceInfo in serviceInfos) {
-                if (serviceInfo.environment == environment) {
-                    return serviceInfo.host!!
-                }
-            }
-        }
-        throw EnvironmentNotFoundException(environment)
-    }
+    throw EnvironmentNotFoundException(environment)
+  }
 }
